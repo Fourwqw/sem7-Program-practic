97ec67d9f4e5de3dc6452a6bb20036cb248ae77e
Fourwqw <54942531+Fourwqw@users.noreply.github.com>
2021-11-25 23:07:52 +0300

    #

---
+++ lesson4/ProgPracticeLesson4/ProgPracticeLesson4/Controllers/WeatherForecastController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using System.Text.Json;
+
+namespace ProgPracticeLesson4.Controllers {
+    [ApiController]
+    [Route("test")]
+    public class WeatherForecastController: ControllerBase 
+    {
+        [HttpPost]
+        [HttpGet]
+        [Produces("application/json")]
+        [Route("index")]
+
+        public async Task<IActionResult> Index(){
+
+            string json = JsonSerializer.Serialize(new { result = "Hello world!" });
+            return this.Content(json);
+        }
+        [HttpPost]
+        [HttpGet]
+        [Produces("application/json")]
+        [Route("hello_world")]
+        public async Task<IActionResult> Test() 
+        {
+            MyClassResponse res = new MyClassResponse();
+            res.Success = "success";
+            res.Result = "Heelo_world";
+            res.Version = "1.0";
+            string json = JsonSerializer.Serialize(res);
+        return this.Content( json );
+        }
+    }
+}
