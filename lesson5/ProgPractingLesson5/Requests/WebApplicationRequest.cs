d517f096df929f52e063750e4c886c0b49f36aed
Fourwqw <54942531+Fourwqw@users.noreply.github.com>
2021-11-29 01:33:44 +0300

    Add files via upload

---
+++ lesson5/ProgPractingLesson5/Requests/WebApplicationRequest.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Linq;
+namespace ProgPractingLesson5.Requests 
+{
+    public class WebApplicationRequest 
+    {
+        public double sideOfBase { get; set; } = 0.0;
+        public double heightOfPyramid { get; set; } = 0.0;
+        public WebApplicationRequest(HttpRequest request) 
+        {
+            var form = request.Form.ToList();
+            foreach( var param in form) 
+            {
+                switch( param.Key.ToLower() ) 
+                {
+                    case "a":
+                        this.sideOfBase = Convert.ToDouble(param.Value.ToArray()[0].ToString());
+                        break;
+                    case "h":
+                        this.heightOfPyramid = Convert.ToDouble(param.Value.ToArray()[0].ToString());
+                        break;
+                    default:
+                        break;
+        }
+            }
+        }
+    }
+}
---
+++ lesson5/ProgPractingLesson5/Structures/MyClassResponse.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace ProgPracticeLesson4 {
+    public class MyClassResponse {
+        public string Success { get; set; }
+        public string Version { get; set; }
+        public double Result { get; set; }
+
+    }
+}
