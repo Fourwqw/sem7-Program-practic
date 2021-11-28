3754254021215caa4903f28f473aa367ba0d68ae
Fourwqw <54942531+Fourwqw@users.noreply.github.com>
2021-11-29 01:31:07 +0300

    Add files via upload

---
+++ lesson4/ProgPracticeLesson4/Structures/MyClassResponse.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ProgPracticeLesson4 {
+    public class MyClassResponse {
+        public string Success { get; set; }
+
+        public string Result { get; set; }
+
+        public string Version { get; set; }
+    }
+}
